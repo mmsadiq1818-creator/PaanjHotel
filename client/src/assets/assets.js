a1177bb00ce6d10af4f3967b359f8c8edf69f2a1
Md Sadiq <mmsadiq1818@gmail.com>
2026-04-23 15:17:11 +0530

    change

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -90,7 +90,7 @@ export const exclusiveOffers = [
 
 // Testimonials Dummy Data
 export const testimonials = [
-    { id: 1, name: "Rohit Verma", address: "New Delhi, India", image: "https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=200", rating: 5, review: "I've used many booking platforms before, but none compare to the personalized experience and attention to detail that QuickStay provides." },
+    { id: 1, name: "Minhal Abbas", address: "New Delhi, India", image: "https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=200", rating: 5, review: "I've used many booking platforms before, but none compare to the personalized experience and attention to detail that QuickStay provides." },
     { id: 2, name: "Ilhan Rizvi", address: "Noida, India", image: "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?q=80&w=200", rating: 4, review: "QuickStay exceeded my expectations. The booking process was seamless, and the hotels were absolutely top-notch. Highly recommended!" },
     { id: 3, name: "Priya Sharma", address: "Mumbai, India", image: "https://images.unsplash.com/photo-1701615004837-40d8573b6652?q=80&w=200", rating: 5, review: "Amazing service! I always find the best luxury accommodations through QuickStay. Their recommendations never disappoint!" }
 ];
